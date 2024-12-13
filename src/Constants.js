3ffceec94d5e2b064b806213fab894415d4fd2a7
vigneshax <vsivakumar@affinityx.com>
2024-12-13 17:19:03 +0530

    addded url

--- src/Constants.js
+++ src/Constants.js
@@ -6,7 +6,7 @@ export const themeColors = {
     warning: "#f3e29f",
     background: "#e1e5e3",
   };
-  
+  // https://drive.google.com/file/d/1IzTOCLp96d06TsYpYzmHTLKLJVhVWW10/view?usp=drive_link
 
   // https://drive.google.com/file/d/1p7UYJVwSOfvWm_YKbEPtYt1DnFZCL2jT/view?usp=drive_link
 export   const correctComments = [
